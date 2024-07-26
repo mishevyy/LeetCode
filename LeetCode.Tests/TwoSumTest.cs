046868f430e6beb711112b77e4a59ea6f17b78e1
Ershov Mihail <mishevyy@gmail.com>
2024-07-26 19:43:13 +0300

    решение задачи TwoSum

---
+++ LeetCode.Tests/TwoSumTest.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Tests
+{
+    public class TwoSumTest
+    {
+        [Fact]
+        public void TwoSum_Test()
+        {
+            var solution = new TwoSumSolution();
+            var numes = new int[] { 2, 7, 11, 15 };
+            var target = 9;
+
+            var result = solution.TwoSum(numes, target);
+
+            Assert.Equal([0, 1], result);
+        }
+    }
+}
