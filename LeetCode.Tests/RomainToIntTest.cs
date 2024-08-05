a61314b0c7a285f06a3ad069ca216bb8fda99aa2
Ershov Mihail <mishevyy@gmail.com>
2024-08-05 18:05:16 +0300

    решение задачи Roman to Integer

---
+++ LeetCode.Tests/RomainToIntTest.cs
@@ -0,0 +1,18 @@
+﻿using LeetCodeSolutions.Easy;
+
+namespace LeetCode.Tests
+{
+
+    public class RomainToIntTest
+    {
+        [Fact]
+        public void RomainToInt_Test()
+        {
+            var solution = new RomainToIntSolution();
+            var romainNumber = "X";
+            var result = solution.RomanToInt(romainNumber);
+
+            Assert.Equal(10, result);
+        }
+    }
+}
