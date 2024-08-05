aab52823a2908e61f3561656618b3cbb38508329
Ershov Mihail <mishevyy@gmail.com>
2024-08-05 18:00:52 +0300

    решение задачи Palindrome Number

---
+++ LeetCode.Tests/PalindromNumberTest.cs
@@ -0,0 +1,17 @@
+﻿using LeetCodeSolutions.Easy;
+
+namespace LeetCode.Tests
+{
+    public class PalindromNumberTest
+    {
+        [Fact]
+        public void PalindromNumber_Test()
+        {
+            var solution = new PalindromNumberSolution();
+            var number = 121;
+            var result = solution.IsPalindrome(number);
+
+            Assert.True(result);
+        }
+    }
+}
---
+++ LeetCode/PalindromNumberSolution.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCodeSolutions.Easy
+{
+    public class PalindromNumberSolution
+    {
+        public bool IsPalindrome(int x)
+        {
+            string stringNumber = x.ToString();
+
+            for(int i = 0; i < stringNumber.Length / 2; i++)
+            {
+                if (!stringNumber[i].Equals(stringNumber[stringNumber.Length - i - 1]))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
