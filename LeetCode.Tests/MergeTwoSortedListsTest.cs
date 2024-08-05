b675a0590f3f5c2e4533ecb8e55d3feb5a25054f
Ershov Mihail <mishevyy@gmail.com>
2024-08-05 18:09:07 +0300

    решение задачи Merge Two Sorted Lists

---
+++ LeetCode.Tests/MergeTwoSortedListsTest.cs
@@ -0,0 +1,18 @@
+﻿using LeetCodeSolutions.Easy;
+
+namespace LeetCode.Tests
+{
+    public class MergeTwoSortedListsTest
+    {
+        [Fact]
+        public void MergeTwoLists_Test()
+        {
+            var solution = new MergeTwoSortedListsSolution();
+            var list1 = new ListNode(1);
+            var list2 = new ListNode(2);
+            var result = solution.MergeTwoLists(list1, list2);
+
+            Assert.NotNull(result);
+        }
+    }
+}
