210433b1d5ea35754473bdcf1b0ff29b167a1e68
Ershov Mihail <mishevyy@gmail.com>
2024-08-17 13:02:51 +0300

    решение задачи RemoveDuplicatesFromSortedArray

---
+++ LeetCode.Tests/RemoveDuplicatesFromSortedArrayTest.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Tests
+{
+    public class RemoveDuplicatesFromSortedArrayTest
+    {
+        [Fact]
+        public void TwoSum_Test()
+        {
+            var solution = new RemoveDuplicatesFromSortedArray();
+            var numes = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            var target = 9;
+
+            var result = solution.RemoveDuplicates(numes);
+
+            Assert.Equal(5, result);
+        }
+    }
+}
---
+++ LeetCode/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode
+{
+    public class RemoveDuplicatesFromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int duplicated = nums[0];
+            int insertedIndex = 1;
+
+            for (int i = 1; i < nums.Length; )
+            {
+                if (nums[i] == duplicated) i++;
+                else
+                {
+                    nums[insertedIndex] = nums[i];
+                    duplicated = nums[i];
+                    insertedIndex++;
+                    i++;
+                }
+            }
+            
+            return insertedIndex;
+        }
+    }
+}
